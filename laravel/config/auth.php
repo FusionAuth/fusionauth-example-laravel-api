5fb7134a70e1adee39f2d1d64ce4ed5ac7303a47
Vinícius Campitelli <1877191+vcampitelli@users.noreply.github.com>
2023-05-25 10:00:17 -0300

    :wrench: Changing auth config file to use the JWT provider

--- laravel/config/auth.php
+++ laravel/config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'api',
         'passwords' => 'users',
     ],
 
@@ -36,8 +36,8 @@ return [
     */
 
     'guards' => [
-        'web' => [
-            'driver' => 'session',
+        'api' => [
+            'driver' => 'jwt',
             'provider' => 'users',
         ],
     ],
@@ -61,7 +61,7 @@ return [
 
     'providers' => [
         'users' => [
-            'driver' => 'eloquent',
+            'driver' => 'fusionauth_eloquent',
             'model' => App\Models\User::class,
         ],
 
@@ -90,14 +90,7 @@ return [
     |
     */
 
-    'passwords' => [
-        'users' => [
-            'provider' => 'users',
-            'table' => 'password_reset_tokens',
-            'expire' => 60,
-            'throttle' => 60,
-        ],
-    ],
+    'passwords' => [],
 
     /*
     |--------------------------------------------------------------------------
