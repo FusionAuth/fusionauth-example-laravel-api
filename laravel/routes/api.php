49a4e7c3e3610403c6a00246bf5b997bc44b5bd4
Vinícius Campitelli <1877191+vcampitelli@users.noreply.github.com>
2023-05-25 10:48:13 -0300

    :truck: Adding authentication routes

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use Illuminate\Contracts\Routing\Registrar;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:api'], function (Registrar $router) {
+    $router->group(['prefix' => 'auth'], function (Registrar $router) {
+        $router->post('logout', [AuthController::class, 'logout']);
+        $router->post('me', [AuthController::class, 'me']);
+    });
 });
